cd1915fade6f197e9c5bdb9fc00c341b571dd857
longnq264 <longhb2642002@gmail.com>
2023-10-16 02:26:32 +0700

    main

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    var x = document.querySelector(".myButton");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  function myFunc() {
+    var x = document.querySelector(".btn-menu");
+    if (x.style.display === "none") {
+      console.log("true");
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
